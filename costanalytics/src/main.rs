a6c1b821de8d3bfff8f8bb3c84d145ed9560402e
root <root@DESKTOP-8R781MA.localdomain>
2024-10-13 23:04:13 +0200

    image reader added. but compilation incomplete

--- costanalytics/src/main.rs
+++ costanalytics/src/main.rs
@@ -5,9 +5,16 @@ mod data_capture;
 // mod data_query;
 // mod data_visualize;
 // mod common;
+use image::io::Reader as ImageReader;
 
-fn main() 
-{
-    // This is the main entry point for the application
-    println!("Hello, world!");
+fn main() {
+    // Load the image from a file
+    let img = ImageReader::open("lidl_receipt.jpg")
+        .expect("Failed to open image")
+        .decode()
+        .expect("Failed to decode image");
+
+    // Get the dimensions of the image
+    let dimensions = img.dimensions();
+    println!("Image dimensions: {:?}", dimensions);
 }
--- costanalytics/target/debug/deps/costanalytics.d
+++
@@ -1,8 +0,0 @@
-D:\rust\costanalytics\CostAnalytics\costanalytics\target\debug\deps\costanalytics.exe: src\main.rs src\data_capture\mod.rs src\data_capture\image_parser.rs src\data_capture\pdf_parser.rs
-
-D:\rust\costanalytics\CostAnalytics\costanalytics\target\debug\deps\costanalytics.d: src\main.rs src\data_capture\mod.rs src\data_capture\image_parser.rs src\data_capture\pdf_parser.rs
-
-src\main.rs:
-src\data_capture\mod.rs:
-src\data_capture\image_parser.rs:
-src\data_capture\pdf_parser.rs:
